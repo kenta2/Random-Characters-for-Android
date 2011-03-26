5915429d79d547aa88adf089881c73320a7b98cd
q <q@example.com>
2011-03-26 16:02:29 -0400

    create random chars, copy to clipboard

--- src/caryl_hafod/RandomChars/RandomChars.java
+++ src/caryl_hafod/RandomChars/RandomChars.java
@@ -1,7 +1,11 @@
 package caryl_hafod.RandomChars;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.ClipboardManager;
 import android.widget.Toast;
 
 public class RandomChars extends Activity {
@@ -9,7 +13,14 @@ public class RandomChars extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(getApplicationContext(), "Hello!", Toast.LENGTH_SHORT).show();
+        Random rnd=new Random();
+        StringBuilder s=new StringBuilder("[");
+        for(int i=0;i<8;++i){
+        	s.append(Character.valueOf((char) (rnd.nextInt(26)+97)));
+        }
+        s.append("] "); //the trailing space is for prefixing to Subject lines
+        ((ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE)).setText(s); //deprecated by Android 3.0
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
         finish();
     }
 }
