8c16f13f0a839b473cf10e2744c0e9ce8c837004
q <q@example.com>
2011-03-26 15:03:16 -0400

    fires up an empty black window

--- src/caryl_hafod/RandomChars/RandomChars.java
+++ src/caryl_hafod/RandomChars/RandomChars.java
@@ -8,6 +8,5 @@ public class RandomChars extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
     }
 }
