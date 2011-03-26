2efcedf98635e05b68710377b5471d4785ba8069
q <q@example.com>
2011-03-26 15:15:06 -0400

    simple toast happens

--- src/caryl_hafod/RandomChars/RandomChars.java
+++ src/caryl_hafod/RandomChars/RandomChars.java
@@ -2,11 +2,14 @@ package caryl_hafod.RandomChars;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class RandomChars extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "Hello!", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
